7ae6ab044d6ebe1e0744fc4abd9776b87e91c21e
Darren Oosthuizen <darren.oosthuizen295@gmail.com>
2021-09-22 10:24:54 +0200

    Added Google Analytics

--- Darren Website/src/App.js
+++ Darren Website/src/App.js
@@ -8,6 +8,10 @@ import Home from "./components/Home";
 import PageNotFound from "./components/404";
 import Aux from "./hoc/Auxillary";
 import Layout from "./components/Layout/Layout";
+import ReactGA from "react-ga";
+
+const TRACKING_ID = "G-THHHCB406C";
+ReactGA.initialize(TRACKING_ID);
 
 const App = () => {
   return (
